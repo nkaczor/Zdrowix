92b8e51f99dd1fd877a4e6c7bbb47d4b0cf42f80
kamilkazmierczak <kamil.kazmierczak@hotmail.com>
2016-05-24 14:28:45 +0200

    renderSingleAppointment

--- front/src/views/VisitsView/VisitsView.js
+++ front/src/views/VisitsView/VisitsView.js
@@ -5,17 +5,55 @@ import style from './visits_view.scss';
 import { Button, TextInput,
          PasswordInput, ImageInput,
          Label, Checkbox,
-         Paper, Calendar } from '../../components';
+         Paper, Calendar,Avatar } from '../../components';
+
+import defaultPhoto from '../../../assets/noImage.gif';
 
 export class VisitsView extends Component {
+
   static propTypes = {
 
   };
 
+  renderSingleAppointment() {
+    return (
+      <div className="col-xs-4">
+        <Paper>
+          <div className={ style['visit-container'] }>
+
+            <div className={ style['avatar-container'] }>
+              <Avatar
+                src={ defaultPhoto }
+                size="80px"
+              />
+            </div>
+
+            <div className={ style['info-container'] }>
+              <p>blue blue</p>
+              <p>basdasdsad</p>
+              <p>basdasdsad</p>
+            </div>
+
+            <div className={ style['info-container'] }>
+              data godzina
+            </div>
+
+
+
+
+
+          </div>
+        </Paper>
+      </div>
+    );
+  }
+
+
   render() {
     return (
-      <div>
-      Hi
+      <div className={ style['visits-view'] }>
+
+        { this.renderSingleAppointment() }
 
       </div>
     );
